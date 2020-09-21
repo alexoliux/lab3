47b0458b87d57603bdc5a8d98e50ff2822b31dd7
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2020-09-21 16:46:00 +0300

    Initial commit

---
+++ src/movable/Movable.java
@@ -0,0 +1,11 @@
+
+package movable;
+import java.lang.*;
+
+public interface Movable
+{
+    public void MoveUp();
+    public void  MoveDown();
+    public void  MoveLeft();
+    public void  MoveRight();
+}
